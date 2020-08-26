da3ae2a6cfafc6c90bb2bf71218423ad10999419
ChenLiang <236338364@qq.com>
2020-08-27 01:35:11 +0800

    dom-diff

--- vue-demo/dom-diff/src/index.js
+++ vue-demo/dom-diff/src/index.js
@@ -1,12 +1,12 @@
-console.log('xxxx')
+import { createElement, render, renderDom } from './element'
 
-let a = '666'
+let virtualDom = createElement('ul', { class: 'list' }, [
+  createElement('li', { class: 'item', style: 'color: red' }, ['a']),
+  createElement('li', { class: 'item' }, ['b']),
+  createElement('li', { class: 'item' }, ['c']),
+])
 
-console.log(a)
-
-console.log('ccc')
-console.log('ddd')
-
-let b = `${a} haha`
-
-console.log(b)
+// 将虚拟dom转换为真实dom渲染到页面上
+let el = render(virtualDom)
+renderDom(el, document.body)
+console.log(virtualDom)
