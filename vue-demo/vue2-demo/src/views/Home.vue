ea068ecba08faf996fe4926d1874bb2e91866ba7
ChenLiang <236338364@qq.com>
2020-08-29 00:50:56 +0800

    vue

--- vue-demo/vue2-demo/src/views/Home.vue
+++ vue-demo/vue2-demo/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    {{ a }} + {{ b }} = {{ sum }}
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -13,6 +14,17 @@ export default {
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      a: 1,
+      b: 2,
+    }
+  },
+  computed: {
+    sum() {
+      return this.a + this.b
+    },
   },
 }
 </script>
