8bb486321699bd7feb342396d56402342d6997b9
ChenLiang <236338364@qq.com>
2020-10-09 09:02:51 +0800

    ts-axios

--- TypeScript/demo/01-demo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Person = (function () {
-    function Person(name) {
-        this.name = name;
-    }
-    return Person;
-}());
-var person = new Person('Leon');
-console.log(person.name);
--- TypeScript/demo/01-demo.ts
+++ TypeScript/demo/01-demo.ts
@@ -1,22 +1,7 @@
-class Person {
-  name: string
-  constructor(name: string) {
-    this.name = name
-  }
+let x: number
+initialize()
+console.log(2 * x)
+
+function initialize() {
+  x = 10
 }
-
-const person = new Person('Leon')
-console.log(person.name)
-
-// comments
-
-class SelectGirl<T> {
-  constructor(private girls: T[]) {}
-  getGirl(index: number): T {
-    return this.girls[index]
-  }
-}
-
-// const selectGirl = new SelectGirl(["大脚", "刘英", "晓红"]);
-const selectGirl = new SelectGirl<string>(['大脚', '刘英', '晓红'])
-// const selectGril = new SelectGirl() < string > ['a', 'b', 'c']
