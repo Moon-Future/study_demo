a28c673464ddbfd0ce8a7a3aa4a358be232bc297
Leon <236338364@qq.com>
2020-10-21 18:57:42 +0800

    promise

---
+++ JavaScript/Promise/test.js
@@ -0,0 +1,27 @@
+const Promise = require('./Promise.js')
+
+// console.log(111)
+// const p = new Promise((resolve, reject) => {
+//   console.log(222)
+//   resolve('haha')
+// })
+
+// p.then(res => {
+//   console.log('xxx', res)
+// })
+
+// p.then(res => {
+//   console.log('yyy', res)
+// })
+// console.log(333, p)
+
+new Promise((resolve, reject) => {
+  resolve(new Promise((resolve1, reject1) => {
+    resolve1(666)
+  }))
+}).then(res => {
+  console.log('success', res)
+}, err => {
+  console.log('fail', err)
+})
+
